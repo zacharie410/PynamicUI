669fb580bb6a213af2221116e519b13e1a6926fc
Zacharie Fortin <zacharie410@gmail.com>
2023-07-17 15:45:43 -0400

    styles

---
+++ styles.py
@@ -0,0 +1,19 @@
+# styles.py
+# Usage: from styles import *
+
+# Font styles
+FONT_REGULAR = ("Times New Roman", 14, "normal")
+FONT_BOLD = ("Times New Roman", 14, "bold")
+
+STYLE = {
+    "font": FONT_REGULAR,
+    "fg_color": "black"
+    }
+
+STYLE2 = {
+    "font": FONT_BOLD,
+    "fg_color": "blue"
+    }
+
+# Widget scaling factor
+WIDGET_SCALING_FACTOR = 1.0  # Use 1.0 for normal scaling, adjust as needed
