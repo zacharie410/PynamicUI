f9151b5249a4f3e8e6fe4b1775da00515317a425
Zacharie Fortin <zacharie410@gmail.com>
2023-07-18 02:07:16 -0400

    example

--- example.py
+++ example.py
@@ -1,27 +1,47 @@
-from pynamicui.pynamicui import *
-from styles import *
+
+from pynamicui import (createStylesheet, createDom, createElement, setAppearanceMode)
+
+import customtkinter as tk
+styles = createStylesheet()
+# Create Stylesheet
+# Adding styles dynamically
+styles.addStyle("PrimaryButton", {
+    "font": ("Arial", 14, "normal"),
+    "fg_color": "grey",
+    "bg_color": "blue"
+})
+styles.addStyle("SecondaryButton", {
+    "font": ("Arial", 14, "normal"),
+    "fg_color": "black",
+    "bg_color": "green"
+})
+# Adding a nested style that inherits from "PrimaryButton" and updates "bg_color"
+styles.addNestedStyle("PrimaryButton", "NestedPrimaryButton", {
+    "fg_color": "darkblue",
+})
 
 class NavBar:
     def __init__(self, dom):
         self.dom = dom
-
+ 
         # Create the home navigation button
-        self.navButton = createElement(
+        self.navButton = createElement(self.dom,
             "Button",
-            style=STYLE,
-            props={"text": "Home", "font": FONT_REGULAR, "command": lambda: self.dom.nav("home")},
+            style="PrimaryButton",
+            props={"text": "Home", "command": lambda: self.dom.nav("home")},
             place={"relwidth": 0.5, "relheight": 1, "rely": 0},
         )
         
         # Create the about navigation button
-        self.navButton2 = createElement(
+        self.navButton2 = createElement(self.dom,
             "Button",
+            style="NestedPrimaryButton",
             props={"text": "About", "command": lambda: self.dom.nav("about")},
             place={"relwidth": 0.5, "relheight": 1, "relx": 0.5},
         )
         
         # Create the navigation bar containing the buttons
-        self.navBar = createElement(
+        self.navBar = createElement(self.dom,
             "Frame",
             children=[self.navButton, self.navButton2],
             place={"relwidth": 1, "relheight": 0.2},
@@ -37,14 +57,14 @@ class CounterPage:
         self.dom = dom
 
         # Create the counter button
-        self.counterButton = createElement(
+        self.counterButton = createElement(self.dom,
             "Button",
             props={"text": "Click Me", "command": lambda: self.increment("counter")},
             place={"relwidth": 1, "relheight": 0.2, "rely": 0.3},
         )
 
         # Create the counter label
-        self.counterLabel = createElement(
+        self.counterLabel = createElement(self.dom,
             "Label",
             name="counterLabel",
             props={"text": "0"},
@@ -52,7 +72,7 @@ class CounterPage:
         )
 
         # Create the counter page containing the button and label
-        self.page1 = createElement(
+        self.page1 = createElement(self.dom,
             "Frame",
             children=[self.counterButton, self.counterLabel],
             place={"relwidth": 1, "relheight": .8, "rely": 0.2},
@@ -99,21 +119,21 @@ class AboutPage:
         self.dom = dom
 
         # Create the about label
-        self.aboutLabel = createElement(
+        self.aboutLabel = createElement(self.dom,
             "Label",
             props={"text": "About page"},
             place={"relwidth": 1, "relheight": 0.2},
         )
         
         # Create the about button
-        self.aboutButton = createElement(
+        self.aboutButton = createElement(self.dom,
             "Button",
             props={"text": "Click Me", "command": lambda: self.action()},
             place={"relwidth": 1, "relheight": 0.2, "rely": 0.3},
         )
 
         # Create the about page containing the label and button
-        self.page2 = createElement(
+        self.page2 = createElement(self.dom,
             "Frame",
             children=[self.aboutLabel, self.aboutButton],
             place={"relwidth": 1, "relheight": .8, "rely": 0.2},
@@ -137,16 +157,17 @@ class AboutPage:
     def action(self):
         if self.aboutLabel.visible:
             self.aboutLabel.hide()
-            self.aboutButton.setStyle(STYLE2)
+            self.aboutButton.setStyle("PrimaryButton")
         else:
             self.aboutLabel.show()
-            self.aboutButton.setStyle(STYLE)
+            self.aboutButton.setStyle("SecondaryButton")
 
 
 class App:
     def __init__(self):
         self.dom = createDom()
-        self.dom.widget.geometry("800x600")
+        self.dom.setStylesheet(styles.stylesheet)
+        self.dom.setGeometry("800x600")
         # Create the navbar component
         self.navbar = NavBar(self.dom)
 
@@ -164,6 +185,7 @@ class App:
         self.dom.render()
 
 
+# Launch app
 # Example usage
 if __name__ == "__main__":
     App()
