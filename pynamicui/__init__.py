c37887fafdb3aea302caaa37f1a9611281bc55a4
Zacharie Fortin <zacharie410@gmail.com>
2023-07-18 02:06:30 -0400

    init package

---
+++ pynamicui/__init__.py
@@ -0,0 +1,14 @@
+import customtkinter as tk
+
+from .createDom import createDom
+from .createElement import createElement
+from .createStylesheet import createStylesheet
+
+def setAppearanceMode(mode):
+    tk.set_appearance_mode(mode)
+
+def setDefaultColorTheme(theme):
+    tk.set_default_color_theme(theme)
+
+def setWidgetScaling(scale):
+    tk.set_widget_scaling(scale)
