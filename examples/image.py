35a9ce316275a19151fcb750b3098c295bbf74e8
Zacharie Fortin <zacharie410@gmail.com>
2023-07-22 22:14:13 -0400

    examples

--- examples/image.py
+++ examples/image.py
@@ -1,6 +1,4 @@
-from pynamicui import createDom, createElement
-from PIL import Image
-import customtkinter
+from pynamicui import createDom, createElement, createImage
 
 class ImageApp:
     def __init__(self, dom):
@@ -10,7 +8,7 @@ class ImageApp:
         self.image_button = createElement(
             self.dom,
             "Button",
-            props={"image": customtkinter.CTkImage(light_image=Image.open("screenshots/counterapp.png"))},
+            props={"image": createImage("screenshots/counterapp.png")},
             place={"relwidth": 0.5, "relheight": 0.5, "relx": 0.25, "rely": 0.25}
         )
 
